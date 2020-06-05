ec7249952629d463cd7b9611722b4218d317b6c2
Corvax <vlvete1990@yandex.ru>
2020-06-05 12:02:29 +0300

    adding testing

---
+++ testing/calc.test.ts
@@ -0,0 +1,14 @@
+import { sum, sub } from './calc.ts';
+//  import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+// import { assertEquals } from "https://deno.land/std@0.53.0/testing/asserts.ts";
+import {assertEquals} from '../dependencies.ts';
+
+Deno.test('Culc function', () => {
+    const result = sum(1, 1);
+    assertEquals(result, 2);
+})
+
+Deno.test('Culc function', () => {
+    const result = sub(2, 1);
+    assertEquals(result, 1);
+})
