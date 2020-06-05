656e9d84302934a754cbeae2999ed78b23731a98
Corvax <vlvete1990@yandex.ru>
2020-06-05 11:38:15 +0300

    adding qrcode file

---
+++ file.ts
@@ -0,0 +1,8 @@
+import { qrcode } from "https://deno.land/x/qrcode/mod.ts";
+const img = await qrcode("http://webtricks-master.ru");
+
+const encoder = new TextEncoder()
+
+const data = encoder.encode(`<img src=${img} />`);
+
+await Deno.writeFile('qrcode.html', data);
